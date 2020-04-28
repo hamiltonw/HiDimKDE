7c773f65a5f2a9f3d9e9ac815dcccd30e780e6d0
jsmarron <50708976+jsmarron@users.noreply.github.com>
2020-04-28 16:18:16 -0400

    25d, Bimodal #1 & 2

--- HidKDE1.m
+++ HidKDE1.m
@@ -3,7 +3,7 @@ disp('Running MATLAB script file HidKDE1.m') ;
 %    Explores High Dimensional Kernel Density Estimation
 %    Using straightforward calculation by MidKDEgaussSM
 
-ipart = 37 ;    %  1 - 2-d example, good h = 1 
+ipart = 35 ;    %  1 - 2-d example, good h = 1 
                %  2 - 2-d example, small h = 0.3
                %  3 - 2-d example, large h = 3
                %  11 - 25-d example, Gaussian, n = 100, Summaries
@@ -69,12 +69,14 @@ if  ipart == 1  | ...
   vxg2 = reshape(mxg',1,100*100) ;
   mxgrid = [vxg1; vxg2] ;
   vkde = HidKDEgaussSM(mdata,mxgrid,h) ;
+  maxkde = max(max(vkde)) ;
   contour(reshape(vkde,100,100)) ;
   hold on ;
     plot([1;100],[1,100],'k--') ;
     plot([1;100],[50,50],'k--') ;
   hold off ;
-  title([titstr ', Contour']) ; 
+  text(5,95,['max = ' num2str(maxkde)],'FontSize',18) ;
+  title([titstr ', Contour'],'FontSize',18) ; 
   orient landscape ;
   pstr = ['HidKDE1p' num2str(ipart) 'Toy2dContour' hstr] ;
   print('-dpsc2',pstr) ;
@@ -86,8 +88,13 @@ if  ipart == 1  | ...
   clf ;
   mxgrid = [1; 1] * linspace(-6,8,400) ;
   [vkde,vkmax,mker] = HidKDEgaussSM(mdata,mxgrid,h) ;
-  plot(mxgrid(1,:)',vkde,'k--','LineWidth',3)
-  title([titstr ', 45 degree slice']) ; 
+  maxkde = max(vkde) ;
+  plot(mxgrid(1,:)',vkde,'k--','LineWidth',3) ;
+  vax = axis ;
+  text(vax(1) + 0.05 * (vax(2) - vax(1)), ...
+       vax(3) + 0.95 * (vax(4) - vax(3)), ...
+       ['max = ' num2str(maxkde)],'FontSize',18) ;
+  title([titstr ', 45 degree slice'],'FontSize',18) ; 
   hold on ;
     plot(mxgrid(1,:)',mker,'b-')
   hold off ;
@@ -102,8 +109,13 @@ if  ipart == 1  | ...
   clf ;
   mxgrid = [linspace(-6,8,400); zeros(1,400)] ;
   [vkde,vkmax,mker] = HidKDEgaussSM(mdata,mxgrid,h) ;
+  maxkde = max(vkde) ;
   plot(mxgrid(1,:)',vkde,'k--','LineWidth',3)
-  title([titstr ', x-axis slice']) ; 
+  vax = axis ;
+  text(vax(1) + 0.05 * (vax(2) - vax(1)), ...
+       vax(3) + 0.95 * (vax(4) - vax(3)), ...
+       ['max = ' num2str(maxkde)],'FontSize',18) ;
+  title([titstr ', x-axis slice'],'FontSize',18) ; 
   hold on ;
     plot(mxgrid(1,:)',mker,'b-')
   hold off ;
@@ -112,7 +124,7 @@ if  ipart == 1  | ...
   print('-dpsc2',pstr) ;
 
 
-  %  Make bar plot of kenel heights
+  %  Make bar plot of kernel heights
   %
   figure(4) ;
   clf ;
@@ -120,7 +132,12 @@ if  ipart == 1  | ...
   mxgrid = [1; 1] * linspace(-6,8,400) ;
   [vkde,vkmax,mker] = HidKDEgaussSM(mdata,mxgrid,h) ;
   bar(vkmax') ;
-  title([titstr ', 45 degree slice, kernel maxima']) ; 
+  maxbar = max(vkmax) ;
+  vax = axis ;
+  text(vax(1) + 0.05 * (vax(2) - vax(1)), ...
+       vax(3) + 0.95 * (vax(4) - vax(3)), ...
+       ['max = ' num2str(maxbar)],'FontSize',18) ;
+  title([titstr ', 45 degree slice, kernel maxima'],'FontSize',18) ; 
   vax = axis ;
   axis([0 (n + 1) 0 vax(4)]) ;
   orient landscape ;
@@ -252,11 +269,14 @@ elseif  ipart == 11  | ...
     clf ;
     titstr = ['Distr''n of Distances to Origin, ' dtitstr] ;
     paramstruct = struct('titlestr',titstr, ...
+                         'titlefontsize',18, ...
                          'xlabelstr','Distances to 0') ;
     kdeSM(vdist0',paramstruct) ;
     hold on ;
       vax = axis ;
       plot(sqrt(25) * ones(2,1),[vax(3) vax(4)],'k--','LineWidth',2) ;
+      vachil = get(gca,'Children') ;
+      set(vachil(3),'Marker','o') ;
     hold off ;
     orient landscape ;
     pstr = ['HidKDE1p' num2str(ipart) diststr 'SumDist0'] ;
@@ -272,6 +292,7 @@ elseif  ipart == 11  | ...
                          'datovlaymin',0.1, ...
                          'datovlaymax',0.9, ...
                          'titlestr',titstr, ...
+                         'titlefontsize',18, ...
                          'xlabelstr','Pairwise Distances') ;
     kdeSM(vpwdist,paramstruct) ;
     hold on ;
@@ -285,6 +306,34 @@ elseif  ipart == 11  | ...
     orient landscape ;
     pstr = ['HidKDE1p' num2str(ipart) diststr 'SumPWDist'] ;
     print('-dpsc2',pstr) ;
+
+
+    %  Make PCA scatterplot
+    %
+    figure(3) ;
+    clf ;
+    [temp, ind] = max(vdist0) ;
+        %    index of data point farthest from origin
+    vdirmax = mdata(:,ind) ;
+    vdirmax = vdirmax / norm(vdirmax) ;
+    titlecellstr = {{'Raw data View' dtitstr}} ;
+    savestr = ['HidKDE1p' num2str(ipart) diststr 'ScatPlotPCA'] ;
+    if ipart == 11 ;
+      paramstruct = struct('npcadiradd',-2, ...
+                           'titlecellstr',titlecellstr, ...
+                           'labelcellstr',{{'MaxDistDir'; ...
+                                            'OPC1 scores'; 'OPC2 scores';}}, ...
+                           'savestr',savestr, ...
+                           'iscreenwrite',1) ;    
+    else ;
+      paramstruct = struct('npcadiradd',2, ...
+                           'titlecellstr',titlecellstr, ...
+                           'labelcellstr',{{'MaxDistDir'; ...
+                                            'PC1 scores'; 'PC2 scores';}}, ...
+                           'savestr',savestr, ...
+                           'iscreenwrite',1) ;    
+    end ;
+    scatplotSM(mdata,vdirmax,paramstruct) ;
 
 
   elseif  ipart == 12  | ...
@@ -386,13 +435,17 @@ elseif  ipart == 11  | ...
     %
     figure(1) ;
     clf ;
-    plot(vsgrid',vkde,'k--','LineWidth',3)
-    title([dtitstr ', x-axis slice']) ; 
+    plot(vsgrid',vkde,'k--','LineWidth',3) ;
+    top = (1.05 * max(vkde)) ;
     hold on ;
       plot(vsgrid',mker,'b-')
+      plot(vscores,linspace(0.4 * top,0.6 * top,n),'go') ;
     hold off ;
-    axis([left right 0 (1.05 * max(vkde))]) ;
-    title([dtitstr ', PC1 slice, h = ' num2str(h)]) ; 
+    axis([left right 0 top]) ;
+    maxkde = max(vkde) ;
+    text(left + 0.05 * (right - left), 0.95 * top, ...
+         ['max = ' num2str(maxkde)],'FontSize',18) ;
+    title([dtitstr ', PC1 slice, h = ' num2str(h)],'FontSize',18) ; 
     orient landscape ;
     pstr = ['HidKDE1p' num2str(ipart) diststr 'PC1slice' hstr] ;
     print('-dpsc2',pstr) ;
@@ -402,9 +455,13 @@ elseif  ipart == 11  | ...
     figure(2) ;
     clf ;
     bar(vkmax') ;
-    title([dtitstr ', PC1 slice, kernel maxima, h = ' num2str(h)]) ; 
+    title([dtitstr ', PC1 slice, kernel maxima, h = ' num2str(h)], ...
+                                                'FontSize',18) ; 
     vax = axis ;
     axis([0 (n + 1) 0 vax(4)]) ;
+    maxk = max(vkmax) ;
+    text(0.05 * (n + 1), 0.95 * vax(4), ...
+         ['max = ' num2str(maxk)],'FontSize',18) ;
     orient landscape ;
     pstr = ['HidKDE1p' num2str(ipart) diststr 'Bars' hstr] ;
     print('-dpsc2',pstr) ;
@@ -415,15 +472,98 @@ elseif  ipart == 11  | ...
     clf ;
     vkde = HidKDEgaussSM(mdata,mdata,h) ;
     bar(vkde') ;
-    title([dtitstr ', heights at data points, h = ' num2str(h)]) ; 
+    title([dtitstr ', heights at data points, h = ' num2str(h)], ...
+                                                'FontSize',18) ;  
     vax = axis ;
     axis([0 (n + 1) 0 vax(4)]) ;
+    maxkde = max(vkde) ;
+    text(0.05 * (n + 1), 0.95 * vax(4), ...
+         ['max = ' num2str(maxkde)],'FontSize',18) ;
     coeffvar = std(vkde) / mean(vkde) ;
-    text((n / 5),0.9 * vax(4), ...
+    text((2 * n / 5),0.9 * vax(4), ...
          ['Coeff of Varn = ' num2str(coeffvar)], ...
-         'Color',[1 0 0]) ;
+         'Color',[1 0 0],'FontSize',18) ;
     orient landscape ;
     pstr = ['HidKDE1p' num2str(ipart) diststr 'Hts' hstr] ;
+    print('-dpsc2',pstr) ;
+
+    %  Make KDEs connecting extreme points
+    %
+    figure(4) ;
+    clf ;
+    vdist0 = sqrt(sum(mdata.^2)) ;
+    [temp, vind] = sort(vdist0) ;
+    vmindat = mdata(:,vind(1)) ;
+    vmindir = vmindat / norm(vmindat) ;
+    vt1dat = mdata(:,vind(round(n / 3))) ;
+    vt1dir = vt1dat / norm(vt1dat) ;
+    vt2dat = mdata(:,vind(round(2 * n / 3))) ;
+    vt2dir = vt2dat / norm(vt2dat) ;
+    vmaxdat = mdata(:,vind(n)) ;
+    vmaxdir = vmaxdat / norm(vmaxdat) ;
+
+    subplot(2,2,1) ;    %  line from 0 to max
+      vproj1 = vmaxdir' * mdata ;
+      left1 = min(vproj1) - 3 * h ;
+      right1 = max(vproj1) + 3 * h ;
+      vsgrid1 = linspace(left1,right1,100) ;
+      mxgrid1 = vmaxdir * vsgrid1 ;
+      vkde1 = HidKDEgaussSM(mdata,mxgrid1,h) ;
+      top1 = (1.05 * max(vkde1)) ;
+      plot(vsgrid1',vkde1,'k','LineWidth',2) ;
+      hold on ;
+        plot(vproj1,linspace(0.4 * top1,0.6 * top1,n),'go') ;
+      hold off ;
+      axis([left1 right1 0 top1]) ;
+      title(['KDE on 0 to max slice, h = ' num2str(h)]) ; 
+
+    subplot(2,2,2) ;    %  line from 0 to Tertile 1
+      vproj2 = vt1dir' * mdata ;
+      left2 = min(vproj2) - 3 * h ;
+      right2 = max(vproj2) + 3 * h ;
+      vsgrid2 = linspace(left2,right2,100) ;
+      mxgrid2 = vt1dir * vsgrid2 ;
+      vkde2 = HidKDEgaussSM(mdata,mxgrid2,h) ;
+      top2 = (1.05 * max(vkde2)) ;
+      plot(vsgrid2',vkde2,'k','LineWidth',2) ; 
+      hold on ;
+        plot(vproj2,linspace(0.4 * top2,0.6 * top2,n),'go') ;
+      hold off ;
+      axis([left2 right2 0 top2]) ;
+      title(['KDE on 0 to T1 slice, h = ' num2str(h)]) ; 
+
+    subplot(2,2,3) ;    %  line from 0 to Tertile 2
+      vproj3 = vt2dir' * mdata ;
+      left3 = min(vproj3) - 3 * h ;
+      right3 = max(vproj3) + 3 * h ;
+      vsgrid3 = linspace(left3,right3,100) ;
+      mxgrid3 = vt2dir * vsgrid3 ;
+      vkde3 = HidKDEgaussSM(mdata,mxgrid3,h) ;
+      top3 = (1.05 * max(vkde3)) ;
+      plot(vsgrid3',vkde3,'k','LineWidth',2) ;
+      hold on ;
+        plot(vproj3,linspace(0.4 * top3,0.6 * top3,n),'go') ;
+      hold off ;
+      axis([left3 right3 0 top3]) ;
+      title(['KDE on 0 to T2 slice, h = ' num2str(h)]) ; 
+
+    subplot(2,2,4) ;    %  line from 0 to min
+      vproj4 = vmindir' * mdata ;
+      left4 = min(vproj4) - 3 * h ;
+      right4 = max(vproj4) + 3 * h ;
+      vsgrid4 = linspace(left4,right4,100) ;
+      mxgrid4 = vmindir * vsgrid4 ;
+      vkde4 = HidKDEgaussSM(mdata,mxgrid4,h) ;
+      top4 = (1.05 * max(vkde4)) ;
+      plot(vsgrid4',vkde4,'k','LineWidth',2)
+      hold on ;
+        plot(vproj4,linspace(0.4 * top4,0.6 * top4,n),'go') ;
+      hold off ;
+      axis([left4 right4 0 top4]) ;
+      title(['KDE on 0 to min slice, h = ' num2str(h)]) ; 
+
+    orient landscape ;
+    pstr = ['HidKDE1p' num2str(ipart) diststr 'AltSlices' hstr] ;
     print('-dpsc2',pstr) ;
 
 
@@ -445,13 +585,19 @@ elseif  ipart == 11  | ...
     [temp,mind] = max(vcv) ;
     figure(1) ;
     clf ;
-    plot(log2(vh),vcv,'r-','LineWidth',3)
-    title([dtitstr ', Coeff''s of Var''n of KDE heights at data']) ;
+    plot(log2(vh),vcv,'r-','LineWidth',3) ;
+    vax = axis ;
+    hold on ;
+      plot([log2(vh(mind)); log2(vh(mind))],[0; vax(4)],'k--') ;
+    hold off ;
+    title([dtitstr ', Coeff''s of Var''n of KDE heights at data'], ...
+                                      'FontSize',18) ;
     xlabel('log_2(h)') ;
     ylabel('Coefficient of Variation') ;
-    vax = axis ;
-    text(2,0.9 * vax(4), ...
-         ['Max at h = ' num2str(vh(mind))]) ;
+    text(log2(vh(mind)) + 0.02 * (log2(max(vh)) - min(log2(vh))), ...
+         0.6 * vax(4), ...
+         ['Max at h = ' num2str(vh(mind))],'FontSize',18) ;
+    axis(vax) ;
     orient landscape ;
     pstr = ['HidKDE1p' num2str(ipart) diststr 'CoeffOfVars'] ;
     print('-dpsc2',pstr) ;
